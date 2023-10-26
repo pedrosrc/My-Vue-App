d5a1a585d99808fa7936d1d4c98b492ae09ec322
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-10-26 18:42:29 -0300

    Add Get Questions and Answers

--- quiz-game-app/src/App.vue
+++ quiz-game-app/src/App.vue
@@ -1,44 +1,102 @@
 <script setup lang="ts">
 
+import { onMounted, reactive, computed, ref } from "vue";
+import axios from 'axios';
 
-// https://opentdb.com/api.php?amount=10
 
-// const questions = []
+let results: any = reactive({
+  questions: "",
+  incorrectAnswers: "",
+  correctAnswer: "",
+  chosenAnswer: undefined,
+  answerSubmitedd: false
+})
 
+const result = ref('')
+
+
+onMounted(async () => {
+  await axios.get("https://opentdb.com/api.php?amount=10")
+    .then((response) => {
+      results.questions = response.data.results[0].question;
+      results.incorrectAnswers = response.data.results[0].incorrect_answers;
+      results.correctAnswer = response.data.results[0].correct_answer;
+    }).catch((error) => {
+      console.log(error)
+    });
+})
+
+const answer = computed(() => {
+  var answer2 = [...results.incorrectAnswers]
+  answer2.splice(Number(Math.round(Math.random() * answer2.length)), 0, results.correctAnswer);
+  return answer2;
+})
+
+
+async function submitAnswer() {
+  if (!results.chosenAnswer) {
+    return alert('Select a item for continue!')
+  } else {
+    const itemSelect = results.chosenAnswer
+    const itemCorrect = results.correctAnswer
+
+    results.answerSubmitedd = true;
+
+    if (itemSelect === itemCorrect) {
+      return result.value = `&#9989 Congratulations. The correct item is: ${itemCorrect}`
+    } else {
+      return result.value = `&#10060; Error. The correct item is: ${itemCorrect}`
+    }
+  }
+
+
+
+}
 
 </script>
 
 <template>
+  <div>
+    <template v-if="answer">
+      <header>
+        <h1>Quiz Game</h1>
+      </header>
 
-  <header>
-      <h1>Quiz Game</h1>
-  </header>
-  
-  <main>
-    <section class="question">
-      <h1>Hello World!</h1>
-    </section>
+      <main>
+        <section class="question">
+          <h1 v-html="results.questions"></h1>
+        </section>
 
-    <section class="items">
-      <input type="radio" name="options" value="true">
-      <label>True</label><br>
+        <template v-for="(answers, index) in answer" :key="index">
+          <div class="items">
+            <input type="radio" name="options" :disabled="results.answerSubmitedd" :value="answers" v-model="results.chosenAnswer">
+            <label v-html="answers"></label><br>
+          </div>
+        </template>
+        <section  v-if="!results.answerSubmitedd" class="result">
+          <button @click="submitAnswer" class="send" type="button">Send</button>
+        </section>
 
-      <input type="radio" name="options" value="false">
-      <label>False</label><br>
+        <section v-if="results.answerSubmitedd" class="result">
+          <span v-html="result"></span>
 
-      <button class="send" type="button">Send</button>
-    </section>
 
-  </main>
+          <button @click="submitAnswer" class="send" type="button">Next Question</button>
+        </section>
+        
 
+        
+
+      </main>
+    </template>
+  </div>
 </template>
 
 <style scoped>
-
 @import url('https://fonts.googleapis.com/css2?family=Inter&family=Josefin+Sans&family=Metal+Mania&family=Russo+One&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Inter&family=Josefin+Sans&family=Metal+Mania&display=swap');
 
-header{
+header {
   display: flex;
   justify-content: center;
   margin: 30px;
@@ -46,7 +104,7 @@ header{
   font-family: 'Russo One', Arial, Helvetica, sans-serif;
 }
 
-header h1{
+header h1 {
   background-color: blue;
   padding: 10px;
   border-radius: 20px;
@@ -55,7 +113,7 @@ header h1{
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 }
 
-main{
+main {
   margin: auto;
   margin-top: 50px;
   min-width: 300px;
@@ -63,21 +121,33 @@ main{
   padding: 20px;
   font-family: 'Josefin Sans', Arial, Helvetica, sans-serif;
 }
-.question{
+
+.question {
   text-align: center;
   font-size: 1.8rem;
+  margin-bottom: 20px;
 }
-.items{
-  margin: 20px;
+
+.items {
+  margin: 5px;
   font-size: 1.2rem;
   text-align: center;
 }
-.items input{
+
+.items input {
   cursor: pointer;
 }
 
-.send{
-  margin: 10px auto;
+.result {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  font-size: 1.2rem;
+  text-align: center;
+}
+
+.send {
+  margin: 20px auto;
   font-family: 'Josefin Sans', Arial, Helvetica, sans-serif;
   cursor: pointer;
   border-radius: 10px;
@@ -89,10 +159,10 @@ main{
   width: 150px;
   font-size: 1.5rem;
 }
-.send:hover{
+
+.send:hover {
   transition: .3s;
   background-color: #00a8e8;
   border-radius: 20px;
 }
-
 </style>
